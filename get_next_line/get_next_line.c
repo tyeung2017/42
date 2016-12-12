f90ff3458517eb157f3ac0065450c393cbe57221
tyeung <leo42us@gmail.com>
2016-12-12 08:27:33 -0800

    in progress

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,4 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int     get_next_line(const int fd, char **line)
+{
+	int		ret;
+	char	*buff;
+	
+	ret = 0;
+	if (BUFF_SIZE < 1 || !(buff = ft_strnew(BUFF_SIZE)))
+		return (-1);
+	if ()
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
 
+# define BUFF_SIZE 1000
+
+int		get_next_line(const int fd, char **line);
+
+#endif
